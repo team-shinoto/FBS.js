08fd410f7d4ecfaf27fc345d407a4bd6b8196ee9
Buntin-Synthia <aokitakumi72@gmail.com>
2022-11-11 15:38:54 +0900

    動作確認

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
+const { Client, Intents } = require("discord.js");
+const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MEMBERS, Intents.FLAGS.GUILD_PRESENCES, Intents.FLAGS.MESSAGE_CONTENT] });
+require("dotenv").config();
 
-//test commit
-//
-import discord from 'discord.js';
+if (process.env.DISCORD_BOT_TOKEN == undefined) {
+    console.error("TOKENが設定されていません。");
+    process.exit(0);
+}
 
-let client = new discord.Client();
-
-client.on('ready', () => {
-	console.log('FBS.js Started');
+client.on("ready", () => {
+    console.log(`ログイン: ${client.user.tag}`);
 });
+
+client.on("interactionCreate", async (interaction) => {
+    if (interaction.mentions.users.has(client.user.id)) {
+        interaction.reply("呼びましたか？");
+        return;
+    }
+});
+
+client.login(process.env.DISCORD_BOT_TOKEN);
