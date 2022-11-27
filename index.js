004b638df13080653831bed3e59bfa1c26a9aea9
RUNFUNRUN <ryotauchiyama1212@gmail.com>
2022-11-27 18:22:03 +0900

    スラッシュコマンド動作確認

--- index.js
+++ index.js
@@ -1,23 +1,60 @@
-const { Client, Intents } = require("discord.js");
-const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MEMBERS, Intents.FLAGS.GUILD_PRESENCES, Intents.FLAGS.MESSAGE_CONTENT] });
-require("dotenv").config();
+const Discord = require("discord.js");
+require('dotenv').config();
 
-const token = process.env.token;
+const commands = {
+    /**
+     *
+     * @param {Discord.CommandInteraction} interaction
+     * @returns
+     */
+    async ping(interaction) {
+        const now = Date.now();
+        const msg = ["pong!", "", `gateway: ${interaction.client.ws.ping}ms`];
+        await interaction.reply({content: msg.join("\n"), ephemeral: true});
+        await interaction.editReply(
+            [...msg, `往復: ${Date.now() - now}ms`].join("\n")
+        );
+        return;
+    },
+    /**
+     *
+     * @param {Discord.CommandInteraction} interaction
+     * @returns
+     */
+    async hello(interaction) {
+        const source = {
+            en(name) {
+                return `Hello, ${name}!`;
+            },
+            ja(name) {
+                return `こんにちは、${name}さん。`;
+            },
+        };
+        const name =
+            interaction.member?.displayName ?? interaction.user.username;
+        const lang = interaction.options.get("language");
+        return interaction.reply(source[lang.value](name));
+    },
 
-if (token == undefined) {
-    console.error("TOKENが設定されていません。");
-    process.exit(0);
-}
+    async valorant(interaction) {
+        const msg = "神ゲー！";
+        return interaction.reply(msg);
+    }
+};
 
-client.on("ready", () => {
-    console.log(`ログイン: ${client.user.tag}`);
-});
-
-client.on("interactionCreate", async (interaction) => {
-    if (interaction.mentions.users.has(client.user.id)) {
-        interaction.reply("呼びましたか？");
+async function onInteraction(interaction) {
+    if (!interaction.isCommand()) {
         return;
     }
+    return commands[interaction.commandName](interaction);
+}
+const client = new Discord.Client({
+    intents: 0,
 });
-
-client.login(token);
+client.on("interactionCreate", (interaction) =>
+    onInteraction(interaction).catch((err) => console.error(err))
+);
+client.login(process.env.token).catch((err) => {
+    console.error(err);
+    process.exit(-1);
+});
