4203484930046acbf534e9d93a49e91f8503cdd1
BuntinWindows <aokitakumi72@gmail.com>
2022-11-29 05:07:47 +0900

    とりあえず

---
+++ test.js
@@ -0,0 +1,5 @@
+
+const {printMsg, hello} = require('./todo.js');
+
+printMsg();
+hello('world');
---
+++ todo.js
@@ -0,0 +1,19 @@
+//todo.js 
+// スラッシュコマンド用関数
+// create by 青木拓海^^
+const fs = require('fs');
+
+const createTodo = () => {
+    let todo = {
+        "id": 0,
+        "name": "test",
+        "subject": "test",
+        "done": false,
+        "userID": "test",
+    }
+
+}
+
+module.exports = {
+
+}
